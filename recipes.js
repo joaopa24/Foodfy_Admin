e53faf56dcaa3e295e44969e7f96e7db47f1b477
joknz21 <joaokn129@gmail.com>
2020-11-20 23:33:37 -0300

    20/11

--- recipes.js
+++ recipes.js
@@ -3,67 +3,79 @@ const data = require("./data.json")
 const { json } = require("express")
 
 exports.home = function (req, res) {
-    return res.render("home", {items:data.recipes})
+    return res.render("home", { items: data.recipes })
 }
 exports.recipes = function (req, res) {
-    return res.render("receitas" , {items:data.recipes})
+    return res.render("receitas", { items: data.recipes })
 }
 exports.about = function (req, res) {
     return res.render("sobre")
 }
 exports.recipe = function (req, res) {
     const id = req.params.id;
-    const Recipes = data.recipes.find(function(recipe){
-      if ( recipe.id == id){
-          return true
-      }
-      if (!recipe.id){
-          return res.send("video not found")
-      }
+    const Recipes = data.recipes.find(function (recipe) {
+        if (recipe.id == id) {
+            return true
+        }
+        if (!recipe.id) {
+            return res.send("video not found")
+        }
     })
 
     const recipe = {
         ...Recipes,
-        
+
     }
-    return res.render("Receita" , { recipe })
+    return res.render("Receita", { recipe })
 }
 exports.index = function (req, res) {
-    return res.render("Admin/index", {recipes:data.recipes})
+    return res.render("Admin/index", { recipes: data.recipes })
 }
 exports.create = function (req, res) {
     return res.render("Admin/create")
 }
-exports.recipe_admin = function(req,res){
+exports.recipe_admin = function (req, res) {
     const id = req.params.id;
-    const Recipes = data.recipes.find(function(recipe){
-      if ( recipe.id == id){
-          return true
-          
-      }
-      if (!recipe.id){
-          return res.send("video not found")
-      }
+    const Recipes = data.recipes.find(function (recipe) {
+        if (recipe.id == id) {
+            return true
+
+        }
+        if (!recipe.id) {
+            return res.send("video not found")
+        }
     })
-    
+
     const recipe = {
-        ...Recipes, 
+        ...Recipes,
     }
-    
-    return res.render("Admin/recipe" , { recipe })
+
+    return res.render("Admin/recipe", { recipe })
 }
 exports.recipe_admin_edit = function (req, res) {
-    return res.render("Admin/edit", {items:data.recipes})
+    const { id } = req.params
+    const foundrecipe = data.recipes.find(function (recipe) {
+        return recipe.id == id
+    })
+    if (!foundrecipe) {
+        return res.send("Professor não encontrado")
+    }
+
+    const recipe = {
+        ...foundrecipe
+    }
+
+    return res.render(`Admin/edit`, { recipe })
 }
-exports.post = function (req,res) {
+exports.post = function (req, res) {
     const keys = Object.keys(req.body)
-    for(key of keys){
-        if(req.body[key] == "")
-        return res.send("porfavor preencha todos os campos")
+    for (key of keys) {
+        if (req.body[key] == "")
+            return res.send("porfavor preencha todos os campos")
     }
 
 
-    let {image_url,title,description,Id_Url,featured,ingredients,preparation,textarea} = req.body
+    let { image_url, title, description, Id_Url, featured, ingredients, preparation, textarea } = req.body
     let id = Id_Url
     let image = image_url
 
@@ -73,15 +85,51 @@ exports.post = function (req,res) {
         ...req.body
     })
 
-    fs.writeFile("data.json", JSON.stringify(data, null,2), function(err){
-    if(err) return res.send("write file error!")
-    return res.redirect(`/admin/Receitas/${id}`)
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+        if (err) return res.send("write file error!")
+        return res.redirect(`/admin/Receitas/${id}`)
     })
 }
-exports.put = function (req,res) {
-    
-}
+exports.put = function (req, res) {
+    const { id } = req.body
+    let index = 0
 
-exports.delete = function (req,res) {
-    return res.send("deletado")
+    const FoundRecipe = data.recipes.find(function (recipe, Foundindex) {
+        if (recipe.id == id) {
+            index = Foundindex
+            return true
+        }
+    })
+    
+    if(!FoundRecipe){
+        return res.send("Receita não encontrada")
+    }
+
+    const Recipe = {
+        ...FoundRecipe,
+        ...req.body
+        /* só funciona se colocar o req.body tbm*/
+    }
+    
+    data.recipes[index] = Recipe
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+        if (err) return res.send("write error!")
+
+        return res.redirect(`/admin/Receitas/${id}`)
+    })
+}
+exports.delete = function (req, res) {
+    const { id } = req.body
+    
+    const filteredrecipe = data.recipes.filter(function (recipe){
+        return recipe.id != id
+    })
+
+    data.recipes = filteredrecipe
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+        if (err) return res.send("write error!")
+
+        return res.redirect("/admin/Receitas")
+    })
 }
--- routes.js
+++ routes.js
@@ -12,7 +12,9 @@ routes.get("/admin/Receitas", recipes.index)
 routes.get("/admin/Receitas/criar", recipes.create)
 routes.get("/admin/Receitas/:id", recipes.recipe_admin)
 routes.get("/admin/Receitas/:id/edit", recipes.recipe_admin_edit)
-
+routes.put("/admin/Receitas", recipes.put)
+routes.delete("/admin/Receitas", recipes.delete)
 routes.post("/admin/Receitas", recipes.post)
+/* Adicionar put e delete */
 
 module.exports = routes
